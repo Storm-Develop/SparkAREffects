40ecdcbdb31281d36fc19a6668676cb35b70dc3d
Vlad <vlad.s@jcaelectronics.ca>
2020-01-07 02:01:23 -0600

    Resized images

--- RandomCartoonChose/scripts/RandomTextGenerator.js
+++ RandomCartoonChose/scripts/RandomTextGenerator.js
@@ -17,20 +17,23 @@ const Time = require('Time');
 var Materials = require('Materials');
 var Textures = require('Textures');
 const Shaders = require('Shaders');
-
-var Reactive = require('Reactive');
-var Animation = require('Animation');
+const CameraInfo = require('CameraInfo');
 
 // Use export keyword to make a symbol available in scripting debug console
 export const Diagnostics = require('Diagnostics');
+var ImageTobeUsed = "";
+
 var BackgroundMainImagePlane = Scene.root.find('BackgroundMain');
 var PlaneRegMaterial = Materials.get('RegularMaterial');
+const isRecordingVideo = CameraInfo.isRecordingVideo;
 
+isRecordingVideo.onOff().subscribe(  
+    function Reset() {
+        ImageSelected =false;
+      });
 
 // Different Textures
 
-
-var ImageTobeUsed = "";
 var ListTextures = [];
 
 var i;
@@ -39,57 +42,36 @@ for (i = 1; i < 51; i++) {
     ListTextures.push(Textures.get(ImageTobeUsed));
 }
 
-//var ListTextures = [BunnyTexture,SimpsonTexture,BartTexture];
 const DiffuseTextureSlot = Shaders.DefaultMaterialTextures.DIFFUSE;
+var RandomCartoonNum;
+var ImageSelected =false;
 
-//BackgroundMainImagePlane.material = Materials.get('RegularMaterial0');
-//PlaneRegMaterial.setTexture(BunnyTexture.signal, {textureSlotName: DiffuseTextureSlot});
-
-
-Time.ms.interval(100).subscribe(
+Time.ms.interval(200).subscribe(
     function(elapsedTime) {
-        var randomCartoonNum = getRandomInt(ListTextures.length);
-        PlaneRegMaterial.setTexture(ListTextures[randomCartoonNum].signal, {textureSlotName: DiffuseTextureSlot});
-
+        if(!ImageSelected)
+        {
+            RandomCartoonNum = getRandomInt(ListTextures.length);
+            PlaneRegMaterial.setTexture(ListTextures[RandomCartoonNum].signal, {textureSlotName: DiffuseTextureSlot});
+        }
         // NOTE: Time.ms may differ slightly from the elapsed
         // time passed to the callback. Time.ms shows the exact
         // time since the effect started, whereas the callback
         // exposes an exact multiple of the specified interval.
        // Diagnostics.log(Time.ms.lastValue);
-
-        // Networking.fetch(url).then(function(result) {
-        //     // Diagnostics.log(result);
-        //     // Log result: {"status":200}
-        //     if ((result.status >= 200) && (result.status < 300)) {
-        //         return result.json();
-        //     }
-        //     throw new Error("HTTP status code " + result.status);
-        // }).then(function(json) {
-        //     var xpos = parseInt(json.x);
-        //     // Diagnostics.log("x: " + xpos + " elapsed time: " + elapsedTime);
-        //     BackgroundMainImagePlane.transform.x = xpos;
-        // }).catch(function(error) {
-        //     Diagnostics.log("There was an issue with fetch operation: " + error.message);
-        // });
     });
 
+
+Time.ms.interval(5000).subscribe(
+        function(elapsedTime) {
+            if (!ImageSelected)
+            {
+                ImageSelected =true;
+                PlaneRegMaterial.setTexture(ListTextures[RandomCartoonNum].signal, {textureSlotName: DiffuseTextureSlot});
+            }
+        });
+    
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
       }
-//console.log(Math.random())
 
-// To use variables and functions across files, use export/import keyword
-// export const animationDuration = 10;
-
-// Use import keyword to import a symbol from another file
-// import { animationDuration } from './script.js'
-
-// To access scene objects
-// const directionalLight = Scene.root.find('directionalLight0');
-
-// To access class properties
-// const directionalLightIntensity = directionalLight.intensity;
-
-// To log messages to the console
-// Diagnostics.log('Console message logged from the script.');
